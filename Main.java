0b32000e1a98fb9e2cccba8997fd91c1aa238e56
abhishek30221 <abhishekgusain2705@gmail.com>
2021-10-19 15:48:30 +0530

    2nd ques

--- Main.java
+++ Main.java
@@ -1,54 +1,57 @@
-import java.io.*; 
-import java.util.*; 
+import java.io.*;
+import java.util.*;
 
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
 
-public class Main
-{
-    public static void main(String[] args) 
-    {
-      Scanner sc = new Scanner(System.in); 
-      MedianFinder m = new MedianFinder(); 
-      int n = 3 , i = 0;  
-      int arr[] = {1 , 2 , 3}; 
-      while(n-- > 0)
-        m.addNum(arr[i++]);
-        System.out.println(m.findMedian());
-    }
-}
- class MedianFinder 
- {
-  PriorityQueue<Integer> pqmax; 
-  PriorityQueue<Integer> pqmin; 
-  
-  public MedianFinder() 
-  {
-      pqmax = new PriorityQueue<>(Collections.reverseOrder()); 
-      pqmin = new PriorityQueue<>(); 
-  }
-  
-  public void addNum(int num) 
-  {
+      Edge(int src, int nbr) {
+         this.src = src;
+         this.nbr = nbr;
+      }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         graph[v1].add(new Edge(v1, v2));
+      }
+
+      boolean[] visited = new boolean[vtces]; 
+      ArrayList<Integer> ans = new ArrayList<>(); 
+      Stack<Integer> st = new Stack<>(); 
+
+      for(int i = 0 ; i < vtces ; i++)
+      {
+        if(visited[i]==false)
+          topodfs(graph , i , st , visited); 
+      }
+      while(st.size() > 0)
+        ans.add(st.pop());
+
+      for(int i : ans)
+        System.out.println(i + " ");  
       
-      if(pqmax.size() == pqmin.size())
+   }
+   public static void topodfs(ArrayList<Edge> graph[] , int src , Stack<Integer> st, boolean[] visited)
+   {
+      visited[src] = true; 
+      for(Edge e : graph[src])
       {
-          pqmin.add(num); 
-          pqmax.add(pqmin.peek()); 
-          pqmin.remove(pqmin.peek());    
+        if(visited[e.nbr]==false)
+          topodfs(graph,e.nbr, st , visited); 
       }
-      else
-      {
-          pqmax.add(num); 
-          pqmin.add(pqmax.peek()); 
-          pqmax.remove(pqmax.peek()); 
-      }
-  }
-  
-  public double findMedian() 
-  {
-       if(pqmax.size() == pqmin.size())
-       {              
-        return (pqmax.peek() + pqmin.peek() * 1.0) / 2.0; 
-       }  
-      return pqmax.peek() * 1.0;
-  }
+      st.push(src);
+   }
 }
