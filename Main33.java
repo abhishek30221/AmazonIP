eef1433e7278de31ed4bc5a5e63f0aea0e22bfff
abhishek30221 <abhishekgusain2705@gmail.com>
2021-10-23 20:45:44 +0530

    33rd and 34th ques

--- Main33.java
+++ Main33.java
@@ -1,46 +1,26 @@
-import java.io.*;
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
-public class Main {
+public class Main33
+{
+    public static  void solve(String str , int n)
+    {
+        int count = 0;
+        int freq[] = new int[26];
 
-   public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int m = Integer.parseInt(br.readLine());
-      int[][] arr = new int[n][m];
+        for(char c : str.toCharArray())
+            freq[c-'a']++;
 
-      for (int i = 0; i < n; i++) {
-         String str = br.readLine();
-         for (int j = 0; j < m; j++) {
-            arr[i][j] = Integer.parseInt(str.split(" ")[j]);
-         }
-      }
+        for(int i = 0 ; i < 26 ; i++)
+            if(freq[i]>1)
+                count++;
 
-      int[][] dp = new int[arr.length][arr[0].length];
-      for (int j = arr[0].length - 1; j >= 0; j--) {
-         for (int i = arr.length - 1; i >= 0; i--) {
-            if (j == arr[0].length - 1) {
-               dp[i][j] = arr[i][j];
-            } else if (i == arr.length - 1) {
-               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
-            } else if (i == 0) {
-               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
-            } else {
-               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i + 1][j + 1], dp[i - 1][j + 1]));
-            }
-         }
-      }
-
-      int max = dp[0][0];
-      for (int i = 1; i < dp.length; i++) {
-         max = Math.max(max, dp[i][0]);
-      }
-      System.out.println(max);
-   }
-
+        System.out.println(count); 
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        String str = br.readLine(); 
+        solve(str , str.length()); 
+    }
 }
-
-
-                        
-                        
-                        
